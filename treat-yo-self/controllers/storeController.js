8ebac2819c5228171b1108de82e1f7808d69b847
zach <zach.r.baker1@gmail.com>
2017-06-20 16:18:30 -0700

    back from london

--- treat-yo-self/controllers/storeController.js
+++ treat-yo-self/controllers/storeController.js
@@ -16,3 +16,16 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  // query the db
+  const stores = await Store.find();
+
+  res.render('stores', { title: 'Stores', stores})
+}
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  // 2. confirm that they are the owner of the store
+  // 3. render out the edit form so the user can edit their store
+}
