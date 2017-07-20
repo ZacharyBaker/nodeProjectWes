214afe884056cd6e78266316d36d47c66874c042
zach <zach.r.baker1@gmail.com>
2017-07-20 06:47:52 -0700

    geospactial ajax endpoint

--- treat-yo-self/controllers/storeController.js
+++ treat-yo-self/controllers/storeController.js
@@ -133,7 +133,23 @@ exports.searchStores = async (req, res) => {
   res.json(stores);
 }
 
+exports.mapStores = async (req, res) => {
+  const coordinates = [req.query.lng, req.query.lat].map(parseFloat)
+  const q = {
+    location: {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates
+        },
+        $maxDistance: 100000 //10km
+      }
+    }
+  }
 
+  const stores = await Store.find(q).select('slug name description location').limit(10);
+  res.json(stores)
+}
 
 
 
--- treat-yo-self/routes/index.js
+++ treat-yo-self/routes/index.js
@@ -55,6 +55,7 @@ router.post('/account/reset/:token',
 ///////
 
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
 
