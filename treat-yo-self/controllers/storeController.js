a9bd4b6fb745eee1ae79e58a4acf69099213cf72
zach <zach.r.baker1@gmail.com>
2017-07-24 06:00:56 -0700

    pagination

--- treat-yo-self/controllers/storeController.js
+++ treat-yo-self/controllers/storeController.js
@@ -57,10 +57,28 @@ exports.createStore = async (req, res) => {
 
 // get all stores
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit) - limit;
   // query the db
-  const stores = await Store.find();
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
 
-  res.render('stores', { title: 'Stores', stores})
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash('info', `Hey you asked for page ${page} and that doesn't exist! So I put you on page ${pages}.`)
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+
+  res.render('stores', { title: 'Stores', stores, page, pages, count})
 }
 
 const confirmOwner = (store, user) => {
